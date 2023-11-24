dd86ff474ea8aecb55ce68f4c0773fe146d30a7d
Hasan Nomaan <hasannoman31@gmail.com>
2023-11-24 13:41:57 +0600

    fixed deployment error

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -3,14 +3,14 @@ import userController from './user.controller';
 
 const UserRoutes = Router();
 
-//user Create,Read, Update and Delete routes
+//user Create,Read,Update and Delete routes
 UserRoutes.post('/', userController.createNewUser);
 UserRoutes.get('/', userController.getAllUsers);
 UserRoutes.get('/:userId', userController.getSingleUser);
 UserRoutes.put('/:userId', userController.updateExistingUser);
 UserRoutes.delete('/:userId', userController.deleteExistingUser);
 
-//order routes
+// order routes 
 UserRoutes.put('/:userId/orders', userController.createOrder);
 UserRoutes.get('/:userId/orders', userController.getOrders);
 UserRoutes.get('/:userId/orders/total-price', userController.totalPrice);
