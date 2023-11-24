53ee7de15cab0d54eb5a26c8e65599a7bf982acd
Hasan Nomaan <hasannoman31@gmail.com>
2023-11-25 00:12:30 +0600

    fix error

--- dist/server.js
+++ dist/server.js
@@ -12,6 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// import app from './app.js';
 const app_js_1 = __importDefault(require("./app.js"));
 const config_1 = __importDefault(require("./app/config"));
 const mongoose_1 = __importDefault(require("mongoose"));
